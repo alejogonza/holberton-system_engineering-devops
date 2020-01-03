7580df335029c0ed449144831d16381f6664325d
alejogonza <alejo.1996.2001@gmail.com>
2020-01-03 00:56:59 +0000

    [feature] add all

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash                                                                                                                              
+#Install nginx                                                                                                                                   
+
+exec {'Install nginx':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "42i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
+    provider => shell,
+    }
